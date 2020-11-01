c15dccd732ad2c0b8d8bd0596d73d368e6e22e4a
endroni <rubao.rcs@gmail.com>
2020-11-01 17:17:08 -0300

    estudando

--- JavaScript_DOM/Exercícios/detran/js/script.js
+++ JavaScript_DOM/Exercícios/detran/js/script.js
@@ -1,14 +1,54 @@
-const velocidadeVia = document.querySelector('#velocidadeVia')
-const btnVia = document.querySelector('#btnVia')
+window.addEventListener('load', start); // Carrega a página antes de executar qualquer aplicação
 
+//Escopo global - Captura os elementos
+const velocidadeVia = document.querySelector('#velocidadeVia')
 const velocidadeTraf = document.querySelector('#velocidadeTraf')
-const btnTraf = document.querySelector('#btnTraf')
+
+const verificar = document.querySelector('#verificar')
 
 const res = document.querySelector('#res')
 
-btnVia.addEventListener('click', Verificar)
-
-function Verificar () {
-    res.textContent = velocidadeVia.value
+function start() {
+    previneEnvio();
 }
 
+verificar.addEventListener('click', Verificar)
+
+function Verificar() {  
+
+    const velMaxima = velocidadeVia.value
+    const odometro = velocidadeTraf.value
+    const diferenca = odometro - velMaxima
+    const porcentagem = diferenca / 100 * velMaxima
+
+    if ( odometro > velMaxima ) {
+        if ( diferenca <= 7 ) {
+            res.textContent = `Advertência! 
+            Velocidade máxima da via ${velMaxima} Km/h.
+            Velocidade trafegada ${odometro} Km/h. `
+        }   
+        else if (porcentagem <= 20 ) {
+            res.textContent = 
+            `Autuado. Infração Grave. 
+            Velocidade máxima da via ${velMaxima} Km/h.
+            Velocidade trafegada ${odometro} Km/h. 
+            ${porcentagem} % acima da velocidade máxima`
+        }
+    }
+    else {
+        res.textContent = 
+        `Velocidade máxima da via ${velMaxima} Km/h.
+        Velocidade trafegada ${odometro} Km/h. 
+        Siga com cuidado`
+    }
+    
+
+    function calculaAutuacao(event) {
+        res.textContent = `teste`
+    }
+    
+      function startValues() {
+        inputSalario.addEventListener('change', calculaAutuacao);
+      }    
+      startValues();
+}
